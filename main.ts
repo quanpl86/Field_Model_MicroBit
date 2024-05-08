d036fdc06316c7c2d77e2fbd1954444e3f0717ad
Long Quân <34340053+quanpl86@users.noreply.github.com>
2024-05-08 14:31:20 +0700

    Mô tả các thành phần có trong sản phẩm.

--- main.ts
+++ main.ts
@@ -10,6 +10,33 @@ input.onButtonPressed(Button.B, function () {
     magicbit.StepperDegree(magicbit.Steppers.STPM1, Degrees)
     music.play(music.tonePlayable(523, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
 })
+/**
+ * Các tính năng của mô hình Hospital:
+ * 
+ * - Nâng hạ thang máy: Dựa vào điều khiển chuyển động của động cơ bước (STEP Motor).
+ * 
+ *    + Nhấn nút A trên mạch Micro:Bit để nâng thang máy.
+ * 
+ *    + Nhấn nút B trên mạch Micro:Bit để hạ thang máy.
+ * 
+ * - Hiển thị trên màn hình LCD 16X4
+ * 
+ *    + Màn hình LCD hiển thị văn bản:
+ * 
+ *     ->   Welcome To  <-
+ * 
+ *       TEKY HOSPITAL
+ * 
+ * - Đèn tự động: Tắt/ mở theo điều kiện ánh sáng từ môi trường với cảm biến ánh sáng.
+ * 
+ * ====================================================================
+ * 
+ * - Cài đặt thư viện mở rộng để lập trình cho mạch mở rộng: mạch Magic:bit
+ * 
+ * magicbit=github:emakefun/pxt-magicbit.
+ * 
+ * - Thông tin về thư viện: https://github.com/emakefun/pxt-magicbit/tree/ee1b367329051c8b117ca4deb5f3a467e8d12720
+ */
 let Degrees = 0
 Degrees = 0
 let strip = neopixel.create(DigitalPin.P1, 6, NeoPixelMode.RGB)
